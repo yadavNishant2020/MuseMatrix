f0973d41df12312e1eedd90d03633511ad7633a0
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-12-21 00:36:17 +0530

    Added schema
    - subscription schema
    - user schema
    - fixed supabase error

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, uuid, timestamp, text } from "drizzle-orm/pg-core"
+import { pgTable, uuid, timestamp, text, jsonb, integer, boolean } from "drizzle-orm/pg-core"
+import { prices, subscriptionStatus } from "../../../migrations/schema";
+import { sql } from "drizzle-orm"
 
 export const workspaces = pgTable('workspaces', {
     id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -8,7 +10,7 @@ export const workspaces = pgTable('workspaces', {
     }),
     workspaceOwner: uuid('workspace_owner').notNull(),
     title: text('title').notNull(),
-    inconId: text('icon_id').notNull(),
+    iconId: text('icon_id').notNull(),
     data: text('data'),
     inTrash: text('in_trash'),
     logo: text('logo'),
@@ -24,7 +26,7 @@ export const folders = pgTable('folders', {
     }),
     workspaceOwner: uuid('workspace_owner').notNull(),
     title: text('title').notNull(),
-    inconId: text('icon_id').notNull(),
+    iconId: text('icon_id').notNull(),
     data: text('data'),
     inTrash: text('in_trash'),
     logo: text('logo'),
@@ -41,7 +43,7 @@ export const files = pgTable('files', {
     }),
     workspaceOwner: uuid('workspace_owner').notNull(),
     title: text('title').notNull(),
-    inconId: text('icon_id').notNull(),
+    iconId: text('icon_id').notNull(),
     data: text('data'),
     inTrash: text('in_trash'),
     logo: text('logo'),
@@ -51,3 +53,21 @@ export const files = pgTable('files', {
     }),
     folderId: uuid('folder_id').references(() => folders.id, { onDelete: 'cascade' })
 })
+
+export const subscriptions = pgTable("subscriptions", {
+    id: text("id").primaryKey().notNull(),
+    userId: uuid("user_id").notNull(),
+    status: subscriptionStatus("status"),
+    metadata: jsonb("metadata"),
+    priceId: text("price_id").references(() => prices.id),
+    quantity: integer("quantity"),
+    cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+    created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+    endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+    trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+});
